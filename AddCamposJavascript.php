7d9f04bab730bfeb9ab80db8c1aaa9ddc9607dde
Kike <kike@areavisual.net>
2017-02-15 19:28:05 +0100

    Incorporo css y js bootstrap
    Descargo los archivos correspondientes a bootstrap descomprimidos para
    poder modificar.

--- AddCamposJavascript.php
+++ AddCamposJavascript.php
@@ -6,8 +6,8 @@
         <meta charset="UTF-8">
         <title>Trabaja con nosotros</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <script src="js/bootstrap.js"></script>
 
         <link rel="stylesheet" href="css/normalize.css">
         <!-- <link rel="stylesheet" href="css/style.css"> -->
